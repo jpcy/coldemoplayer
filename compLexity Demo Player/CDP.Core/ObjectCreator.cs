fafeda4dd62c9ef0775160de2ffa8301058e7ca4
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-09-10 09:10:26 +0000

    [2.0] Added IOC helper class ObjectCreator.

---
+++ compLexity Demo Player/CDP.Core/ObjectCreator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CDP.Core
+{
+    public interface IObjectProvider
+    {
+        T Get<T>(params object[] args);
+    }
+
+    public static class ObjectCreator
+    {
+        private class Mapping
+        {
+            public Type Type { get; private set; }
+            public IObjectProvider Provider { get; private set; }
+
+            public Mapping(Type type)
+            {
+                Type = type;
+            }
+
+            public Mapping(IObjectProvider provider)
+            {
+                Provider = provider;
+            }
+        }
+
+        private static Dictionary<Type, Mapping> mappings = new Dictionary<Type, Mapping>();
+
+        public static void Map<T1,T2>()
+        {
+            mappings.Add(typeof(T1), new Mapping(typeof(T2)));
+        }
+
+        public static void MapToProvider<T>(IObjectProvider provider)
+        {
+            mappings.Add(typeof(T), new Mapping(provider));
+        }
+
+        public static T Get<T>(params object[] args)
+        {
+            Mapping mapping = mappings[typeof(T)];
+
+            if (mapping.Provider != null)
+            {
+                return mapping.Provider.Get<T>(args);
+            }
+
+            return (T)Activator.CreateInstance(mapping.Type, args);
+        }
+
+        public static void Reset()
+        {
+            mappings.Clear();
+        }
+    }
+}
