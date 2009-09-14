156c30fd4c3ec9cc3d2a73a4e917b5b138f60655
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-09-14 05:04:44 +0000

    [2.0] Generic progress view (progress bar and cancel button).

---
+++ compLexity Demo Player/CDP/ViewModel/Progress.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CDP.ViewModel
+{
+    public class Progress : Core.ViewModelBase
+    {
+        public event EventHandler CancelEvent;
+
+        private int _value = 0;
+        public int Value
+        {
+            get { return _value; }
+            set
+            {
+                _value = value;
+                OnPropertyChanged("Value");
+            }
+        }
+
+        public DelegateCommand CancelCommand { get; private set; }
+
+        private readonly INavigationService navigationService = Core.ObjectCreator.Get<INavigationService>();
+
+        public Progress()
+        {
+            CancelCommand = new DelegateCommand(CancelCommandExecute);
+        }
+
+        public void CancelCommandExecute()
+        {
+            OnCancel();
+            navigationService.Home();
+        }
+
+        private void OnCancel()
+        {
+            if (CancelEvent != null)
+            {
+                CancelEvent(this, EventArgs.Empty);
+            }
+        }
+    }
+}
