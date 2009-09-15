f0a93d775e21be975c61e8ea5968ac7bb3cf6c93
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-09-15 05:24:03 +0000

    [2.0] Added RichTextBox wrapper for Document data-binding.

---
+++ compLexity Demo Player/CDP.Core/RichTextBox.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace CDP.Core
+{
+    public partial class RichTextBox : UserControl
+    {
+        public FlowDocument Document
+        {
+            get { return (FlowDocument)GetValue(DocumentProperty); }
+            set { SetValue(DocumentProperty, value); }
+        }
+
+        public static DependencyProperty DocumentProperty = DependencyProperty.Register("Document", typeof(FlowDocument), typeof(RichTextBox), new PropertyMetadata(OnDocumentChanged));
+
+        public RichTextBox()
+        {
+            InitializeComponent();
+        }
+
+        public static void OnDocumentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((RichTextBox)d).childRichTextBox.Document = (FlowDocument)e.NewValue ?? new FlowDocument();
+        }
+    }
+}
