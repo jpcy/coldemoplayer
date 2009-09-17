9489960329e70965c6a71c7981bbfe6c7d92c9cd
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-09-17 01:10:40 +0000

    [2.0] RemoveChars string extension.

---
+++ compLexity Demo Player/CDP.Core.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using CDP.Core.Extensions;
+
+namespace CDP.Core.Tests.Extensions
+{
+    [TestFixture]
+    public class StringExtensionsTests
+    {
+        [Test]
+        public void RemoveChars_Ok()
+        {
+            string input = "abcdefghijklm";
+            Assert.That(input.RemoveChars('c', 'h', 'm'), Is.EqualTo("abdefgijkl"));
+        }
+
+        [Test]
+        public void RemoveChars_NoChars()
+        {
+            string input = "abc";
+            Assert.That(input.RemoveChars(), Is.EqualTo(input));
+        }
+
+        [Test]
+        public void RemoveChars_EmptyInput()
+        {
+            string input = string.Empty;
+            Assert.That(input.RemoveChars('a', 'b', 'c'), Is.EqualTo(input));
+        }
+
+        [Test]
+        public void RemoveChars_EmptyResult()
+        {
+            string input = "abc";
+            Assert.That(input.RemoveChars('a', 'b', 'c'), Is.EqualTo(string.Empty));
+        }
+    }
+}
---
+++ compLexity Demo Player/CDP.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace CDP.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string RemoveChars(this string s, params char[] chars)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!chars.Contains(s[i]))
+                {
+                    sb.Append(s[i]);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
