4979ecba2ec1c6925bca0061b176df72fef932e1
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-10-03 12:56:18 +0000

    [2.0] HL demo: svc_roomtype message.

---
+++ compLexity Demo Player/CDP.HalfLifeDemo/Messages/SvcRoomType.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using BitReader = CDP.Core.BitReader;
+using BitWriter = CDP.Core.BitWriter;
+
+
+namespace CDP.HalfLifeDemo.Messages
+{
+    public class SvcRoomType : EngineMessage
+    {
+        public override byte Id
+        {
+            get { return (byte)EngineMessageIds.svc_roomtype; }
+        }
+
+        public override string Name
+        {
+            get { return "svc_roomtype"; }
+        }
+
+        public override bool CanSkipWhenWriting
+        {
+            get { return true; }
+        }
+
+        public short RoomType { get; set; }
+
+        public override void Skip(BitReader buffer)
+        {
+            buffer.SeekBytes(2);
+        }
+
+        public override void Read(BitReader buffer)
+        {
+            RoomType = buffer.ReadShort();
+        }
+
+        public override void Write(BitWriter buffer)
+        {
+            buffer.WriteShort(RoomType);
+        }
+
+        public override void Log(StreamWriter log)
+        {
+            log.WriteLine("Room type: {0}", RoomType);
+        }
+    }
+}
