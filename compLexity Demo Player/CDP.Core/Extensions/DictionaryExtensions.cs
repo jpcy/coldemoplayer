cc35511acb85279c541b6d2cafa41538953eb0a7
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-10-07 02:24:33 +0000

    [2.0] AddRange dictionary extension method.

---
+++ compLexity Demo Player/CDP.Core/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CDP.Core.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static void AddRange<TKey, TValue>(this Dictionary<TKey, TValue> self, IDictionary<TKey, TValue> dictionary)
+        {
+            foreach (var kvp in dictionary)
+            {
+                self.Add(kvp.Key, kvp.Value);
+            }
+        }
+    }
+}
