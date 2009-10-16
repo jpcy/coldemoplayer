c76949f6a330a0c3a16768e7a1afedf788be0f82
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-10-16 11:05:10 +0000

    [2.0] Added CyclicQueue

---
+++ compLexity Demo Player/CDP.Core/CyclicQueue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace CDP.Core
+{
+    public class CyclicQueue<T> : IEnumerable<T>, IEnumerable
+    {
+        private readonly int maximumSize;
+        private readonly Queue<T> queue = new Queue<T>();
+
+        public int Count
+        {
+            get { return queue.Count; }
+        }
+
+        public CyclicQueue(int maximumSize)
+        {
+            this.maximumSize = maximumSize;
+        }
+
+        public void Enqueue(T item)
+        {
+            queue.Enqueue(item);
+
+            if (queue.Count > maximumSize)
+            {
+                queue.Dequeue();
+            }
+        }
+
+        public void Clear()
+        {
+            queue.Clear();
+        }
+
+        public Queue<T>.Enumerator GetEnumerator()
+        {
+            return queue.GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return queue.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return queue.GetEnumerator();
+        }
+    }
+}
