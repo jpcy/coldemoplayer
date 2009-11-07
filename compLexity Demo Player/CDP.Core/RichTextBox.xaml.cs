6432b1ca5b2dfb0629510fc00422ebc7ec84d41e
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-11-07 11:41:32 +0000

    [2.0] Added FontFamily and Background dependency properties to RichTextBox wrapper.

--- compLexity Demo Player/CDP.Core/RichTextBox.xaml.cs
+++ compLexity Demo Player/CDP.Core/RichTextBox.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace CDP.Core
 {
@@ -13,7 +14,23 @@ namespace CDP.Core
             set { SetValue(DocumentProperty, value); }
         }
 
+        public Brush Background
+        {
+            get { return (Brush)GetValue(BackgroundProperty); }
+            set { SetValue(BackgroundProperty, value); }
+        }
+
+        public FontFamily FontFamily
+        {
+            get { return (FontFamily)GetValue(FontFamilyProperty); }
+            set { SetValue(FontFamilyProperty, value); }
+        }
+
         public static DependencyProperty DocumentProperty = DependencyProperty.Register("Document", typeof(FlowDocument), typeof(RichTextBox), new PropertyMetadata(OnDocumentChanged));
+
+        public static DependencyProperty BackgroundProperty = DependencyProperty.Register("Background", typeof(Brush), typeof(RichTextBox), new PropertyMetadata(OnBackgroundChanged));
+
+        public static DependencyProperty FontFamilyProperty = DependencyProperty.Register("FontFamily", typeof(FontFamily), typeof(RichTextBox), new PropertyMetadata(OnFontFamilyChanged));
 
         public RichTextBox()
         {
@@ -23,6 +40,16 @@ namespace CDP.Core
         public static void OnDocumentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             ((RichTextBox)d).childRichTextBox.Document = (FlowDocument)e.NewValue ?? new FlowDocument();
+        }
+
+        public static void OnBackgroundChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((RichTextBox)d).childRichTextBox.Background = (Brush)e.NewValue;
+        }
+
+        public static void OnFontFamilyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((RichTextBox)d).childRichTextBox.FontFamily = (FontFamily)e.NewValue;
         }
     }
 }
