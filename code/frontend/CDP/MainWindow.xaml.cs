81f40048a59b816bd877e5c3460556615ffc6123
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-11-22 04:03:23 +0000

    [2.0] Revert CDP project MainWindow files.

--- code/frontend/CDP/MainWindow.xaml.cs
+++ code/frontend/CDP/MainWindow.xaml.cs
@@ -1,27 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CDP
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += new RoutedEventHandler(MainWindow_Loaded);
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            CDP.INavigationService service = Core.ObjectCreator.Get<CDP.INavigationService>();
+            service.Window = this;
+            service.Navigate(new View.Main(), new ViewModel.Main());
         }
     }
 }
