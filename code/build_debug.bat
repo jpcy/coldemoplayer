153ed0a32459895cee7807c5d42deaa0d0101a7b
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-11-22 08:08:42 +0000

    [2.0] Added msbuild batch files.

---
+++ code/build_debug.bat
@@ -0,0 +1,8 @@
+@echo off
+%windir%\Microsoft.NET\Framework\v4.0.21006\msbuild.exe /p:Configuration=Debug /verbosity:m /fl /flp:Verbosity=normal core\core.sln
+if %errorlevel% NEQ 0 goto end
+%windir%\Microsoft.NET\Framework\v4.0.21006\msbuild.exe /p:Configuration=Debug /verbosity:m /fl /flp:Verbosity=normal;Append plugins\plugins.sln
+if %errorlevel% NEQ 0 goto end
+%windir%\Microsoft.NET\Framework\v4.0.21006\msbuild.exe /p:Configuration=Debug /verbosity:m /fl /flp:Verbosity=normal;Append frontend\frontend.sln
+:end
+pause
