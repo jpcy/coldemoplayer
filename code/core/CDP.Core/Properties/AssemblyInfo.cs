f245e663a489758160bad4981587abd18aec9d26
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-11-22 01:17:12 +0000

    [2.0] Moved CDP.Core to its own solution.

--- code/core/CDP.Core/Properties/AssemblyInfo.cs
+++ code/core/CDP.Core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0027d05d-cbef-425e-af48-87d6f7ee0962")]
+[assembly: Guid("72fd02c8-b632-48d9-b633-8208a3771f9b")]
 
 // Version information for an assembly consists of the following four values:
 //
