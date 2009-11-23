46846dd28ac88a29b0688b05f264871edc54b21a
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-11-23 03:43:09 +0000

    [2.0] Import CDP.Cli project - simple command line alternative to CDP.Gui for playing demos.

---
+++ code/frontend/CDP.Cli/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using CDP.Core;
+
+namespace CDP.Cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: CDP.Cli filename");
+                Environment.Exit(1);
+            }
+
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("File \'{0}\' doesn't exist.", args[0]);
+                Environment.Exit(1);
+            }
+
+            // Object mappings.
+            Core.ObjectMappings.Initialise();
+
+            // Demo manager and plugins.
+            IDemoManager demoManager = ObjectCreator.Get<IDemoManager>();
+            demoManager.AddPlugin(0, new HalfLife.Handler());
+            demoManager.AddPlugin(1, new CounterStrike.Handler());
+            demoManager.AddPlugin(0, new Quake3Arena.Handler());
+            demoManager.AddPlugin(0, new QuakeLive.Handler());
+
+            // Settings.
+            ISettings settings = ObjectCreator.Get<ISettings>();
+
+            foreach (Setting setting in demoManager.GetAllDemoHandlerSettings())
+            {
+                settings.Add(setting);
+            }
+
+            settings.Load();
+
+            // Create and load demo.
+            Demo demo = demoManager.CreateDemo(args[0]);
+
+            if (demo == null)
+            {
+                Console.WriteLine("No matching plugin found for \'{0}\'.", args[0]);
+                Environment.Exit(1);
+            }
+
+            demo.OperationErrorEvent += demo_OperationErrorEvent;
+            demo.Load();
+
+            // Create and verify launcher
+            Launcher launcher = demoManager.CreateLauncher(demo);
+
+            if (!launcher.Verify())
+            {
+                Console.WriteLine(launcher.Message);
+                Environment.Exit(1);
+            }
+
+            // Write demo.
+            demo.ProgressChangedEvent += demo_ProgressChangedEvent;
+            demo.OperationCompleteEvent += demo_OperationCompleteEvent;
+            Console.Write("Writing demo [");
+            demo.Write(launcher.CalculateDestinationFileName());
+
+            // Launch.
+            // TODO: monitor game process.
+            launcher.Launch();
+        }
+
+        static void demo_OperationErrorEvent(object sender, Demo.OperationErrorEventArgs e)
+        {
+            Console.WriteLine();
+            Console.WriteLine(e.ErrorMessage);
+            Console.WriteLine(e.Exception);
+            Environment.Exit(1);
+        }
+
+        static void demo_ProgressChangedEvent(object sender, Demo.ProgressChangedEventArgs e)
+        {
+            if (e.Progress % 10 == 0)
+            {
+                Console.Write(".");
+            }
+        }
+
+        static void demo_OperationCompleteEvent(object sender, EventArgs e)
+        {
+            Console.WriteLine("]");
+        }
+    }
+}
