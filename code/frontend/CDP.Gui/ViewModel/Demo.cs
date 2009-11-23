8aa399344f380dae6e4de899a29417a3c7048674
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-11-23 03:39:46 +0000

    [2.0] Demo.MapImagesRelativePath null check.

--- code/frontend/CDP.Gui/ViewModel/Demo.cs
+++ code/frontend/CDP.Gui/ViewModel/Demo.cs
@@ -12,7 +12,7 @@ namespace CDP.Gui.ViewModel
         {
             get
             {
-                if (Data == null)
+                if (Data == null || Data.MapImagesRelativePath == null)
                 {
                     return null;
                 }
@@ -24,7 +24,7 @@ namespace CDP.Gui.ViewModel
         {
             get
             {
-                if (Data == null)
+                if (Data == null || Data.MapImagesRelativePath == null)
                 {
                     return null;
                 }
