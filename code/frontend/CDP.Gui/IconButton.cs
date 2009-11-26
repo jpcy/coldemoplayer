be3cb51c3bb2e27fa7ee3a02ba6cf3b57f56c4c0
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-11-26 07:32:38 +0000

    [2.0] IconButton styling.

--- code/frontend/CDP.Gui/IconButton.cs
+++ code/frontend/CDP.Gui/IconButton.cs
@@ -2,31 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Markup;
+using System.Windows;
+using System.Windows.Media;
 
-namespace CDP.Gui.UserControls
+namespace CDP.Gui
 {
     [ContentProperty("Text")]
-    public partial class IconButton : UserControl
+    internal class IconButton : Button
     {
-        public static readonly DependencyProperty CommandProperty;
         public static readonly DependencyProperty IconSourceProperty;
         public static readonly DependencyProperty TextProperty;
-
-        public ICommand Command
-        {
-            get { return (ICommand)GetValue(CommandProperty); }
-            set { SetValue(CommandProperty, value); }
-        }
 
         public ImageSource IconSource
         {
@@ -42,14 +29,8 @@ namespace CDP.Gui.UserControls
 
         static IconButton()
         {
-            CommandProperty = DependencyProperty.Register("Command", typeof(ICommand), typeof(IconButton));
             IconSourceProperty = DependencyProperty.Register("IconSource", typeof(ImageSource), typeof(IconButton));
             TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(IconButton));
-        }
-
-        public IconButton()
-        {
-            InitializeComponent();
         }
     }
 }
