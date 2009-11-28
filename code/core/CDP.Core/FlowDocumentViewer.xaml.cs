e6d5ac03009eb2a7dc992bf200aacfeeeb0bf04c
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-11-28 01:50:05 +0000

    [2.0] Switched FlowDocument host from RichTextBox to FlowDocumentScrollViewer (better performance).

---
+++ code/core/CDP.Core/FlowDocumentViewer.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace CDP.Core
+{
+    public partial class FlowDocumentViewer : UserControl
+    {
+        public FlowDocument Document
+        {
+            get { return (FlowDocument)GetValue(DocumentProperty); }
+            set { SetValue(DocumentProperty, value); }
+        }
+
+        public new Brush Background
+        {
+            get { return (Brush)GetValue(BackgroundProperty); }
+            set { SetValue(BackgroundProperty, value); }
+        }
+
+        public new FontFamily FontFamily
+        {
+            get { return (FontFamily)GetValue(FontFamilyProperty); }
+            set { SetValue(FontFamilyProperty, value); }
+        }
+
+        public new double FontSize
+        {
+            get { return (double)GetValue(FontSizeProperty); }
+            set { SetValue(FontSizeProperty, value); }
+        }
+
+        public static DependencyProperty DocumentProperty = DependencyProperty.Register("Document", typeof(FlowDocument), typeof(FlowDocumentViewer), new PropertyMetadata(OnDocumentChanged));
+
+        public static new DependencyProperty BackgroundProperty = DependencyProperty.Register("Background", typeof(Brush), typeof(FlowDocumentViewer));
+
+        public static new DependencyProperty FontFamilyProperty = DependencyProperty.Register("FontFamily", typeof(FontFamily), typeof(FlowDocumentViewer));
+
+        public static new DependencyProperty FontSizeProperty = DependencyProperty.Register("FontSize", typeof(double), typeof(FlowDocumentViewer));
+
+        public FlowDocumentViewer()
+        {
+            InitializeComponent();
+        }
+
+        public static void OnDocumentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            FlowDocumentViewer viewer = (FlowDocumentViewer)d;
+            FlowDocument document = (FlowDocument)e.NewValue ?? new FlowDocument();
+            document.Background = viewer.Background ?? document.Background;
+            document.FontFamily = viewer.FontFamily ?? document.FontFamily;
+            document.FontSize = viewer.FontSize == 0 ? 14 : viewer.FontSize;
+            document.PagePadding = new Thickness(4);
+            viewer.scrollViewer.Document = document;
+        }
+    }
+}
