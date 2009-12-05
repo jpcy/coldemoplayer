7a43369fa281e140205b2364b8545198d8feaa7a
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-12-05 12:08:14 +0000

    [2.0] Added Drag and Drop.

--- code/frontend/CDP.Gui/Views/Main.xaml.cs
+++ code/frontend/CDP.Gui/Views/Main.xaml.cs
@@ -1,24 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using CDP.Core;
+using System.IO;
 
 namespace CDP.Gui.Views
 {
-    public partial class Main : Page
+    internal partial class Main : Page
     {
         public Main()
         {
             InitializeComponent();
+        }
+
+        private void Page_DragEnter(object sender, DragEventArgs e)
+        {
+            INavigationService navigationService = ObjectCreator.Get<INavigationService>();
+
+            if (navigationService.CurrentPageTitle != "Main")
+            {
+                return;
+            }
+
+            if (e.Data.GetData(DataFormats.FileDrop) != null)
+            {
+                e.Effects = DragDropEffects.Link;
+            }
+        }
+
+        private void Page_Drop(object sender, DragEventArgs e)
+        {
+            string[] fileNames = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (File.Exists(fileNames[0]))
+            {
+                IMediator mediator = ObjectCreator.Get<IMediator>();
+                mediator.Notify(Messages.SetFolder, new SetFolderMessageParameters(Path.GetDirectoryName(fileNames[0]), Path.GetFileName(fileNames[0])));
+            }
         }
     }
 }
