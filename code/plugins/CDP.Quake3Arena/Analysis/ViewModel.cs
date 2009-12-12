38934da96eacab6b10d33c8ed886a5d9f7f3dad0
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-12-12 06:09:11 +0000

    [2.0] Q3A: separate game and console logs.

--- code/plugins/CDP.Quake3Arena/Analysis/ViewModel.cs
+++ code/plugins/CDP.Quake3Arena/Analysis/ViewModel.cs
@@ -10,11 +10,13 @@ using CDP.IdTech3.Commands;
 
 namespace CDP.Quake3Arena.Analysis
 {
-    public class ViewModel : Core.ViewModelBase
+    public class ViewModel : ViewModelBase
     {
         public FlowDocument GameLogDocument { get; private set; }
+        public FlowDocument ConsoleLogDocument { get; private set; }
         private readonly Demo demo;
-        private readonly Core.IFlowDocumentWriter gameLog = Core.ObjectCreator.Get<Core.IFlowDocumentWriter>();
+        private readonly IFlowDocumentWriter gameLog = ObjectCreator.Get<IFlowDocumentWriter>();
+        private readonly IFlowDocumentWriter consoleLog = ObjectCreator.Get<IFlowDocumentWriter>();
         private readonly List<string> playerNames = new List<string>();
         private readonly string chatServerCommandMarker = "chat ";
         private readonly string printServerCommandMarker = "print ";
@@ -42,14 +44,18 @@ namespace CDP.Quake3Arena.Analysis
             demo.AddCommandCallback<SvcServerCommand>(ServerCommand);
             demo.AddCommandCallback<SvcSnapshot>(Snapshot);
             GameLogDocument = new FlowDocument();
+            ConsoleLogDocument = new FlowDocument();
         }
 
         public override void OnNavigateComplete()
         {
             gameLog.Save(GameLogDocument);
+            consoleLog.Save(ConsoleLogDocument);
             OnPropertyChanged("GameLogDocument");
+            OnPropertyChanged("ConsoleLogDocument");
         }
 
+        #region Document writing
         /// <summary>
         /// Checks if a string contains at least one colour escape sequence.
         /// </summary>
@@ -73,9 +79,11 @@ namespace CDP.Quake3Arena.Analysis
         }
 
         /// <summary>
-        /// Writes a string to the game log that uses escaped sequences to indicate text colours.
+        /// Writes a string that uses escaped sequences to indicate text colours to a flow document.
         /// </summary>
-        private void GameLogWriteColourString(string s)
+        /// <param name="document">The document to write to.</param>
+        /// <param name="s">The string to write.</param>
+        private void WriteColouredString(IFlowDocumentWriter document, string s)
         {
             if (IsColourString(s))
             {
@@ -93,7 +101,7 @@ namespace CDP.Quake3Arena.Analysis
 
                         if (runLength > 0)
                         {
-                            gameLog.Write(s.Substring(runStartIndex, runLength), stringColours[colour]);
+                            document.Write(s.Substring(runStartIndex, runLength), stringColours[colour]);
                         }
 
                         // Set the new colour and skip this escape sequence
@@ -113,7 +121,7 @@ namespace CDP.Quake3Arena.Analysis
                         // End of string, write the current run up to the end.
                         if (runStartIndex < s.Length - 1)
                         {
-                            gameLog.Write(s.Substring(runStartIndex), stringColours[colour]);
+                            document.Write(s.Substring(runStartIndex), stringColours[colour]);
                         }
 
                         break;
@@ -122,12 +130,25 @@ namespace CDP.Quake3Arena.Analysis
             }
             else
             {
-                gameLog.Write(s, Brushes.White);
+                document.Write(s, Brushes.White);
             }
 
-            gameLog.WriteLine();
+            document.WriteLine();
         }
 
+        /// <summary>
+        /// Write a server command to a flow document, stripping the command marker and trimming leading and trailing quotes and newline characters.
+        /// </summary>
+        /// <param name="document">The document to write to.</param>
+        /// <param name="commandMarker">The command marker to strip from the command (e.g. 'chat ').</param>
+        /// <param name="command">The command.</param>
+        private void WriteServerCommand(IFlowDocumentWriter document, string commandMarker, string command)
+        {
+            WriteColouredString(document, command.Remove(0, commandMarker.Length).Trim('"', '\n'));
+        }
+        #endregion
+
+        #region Player obituary
         /// <summary>
         /// Calculate an obituary message for a player's death.
         /// </summary>
@@ -281,6 +302,130 @@ namespace CDP.Quake3Arena.Analysis
         }
 
         /// <summary>
+        /// Calculate an simplified obituary message for a player's death.
+        /// </summary>
+        /// <param name="mod">The player's means of death.</param>
+        /// <param name="killerIndex">The player index of the killer.</param>
+        /// <param name="victimIndex">The player index of the victim.</param>
+        /// <returns>An obituary message.</returns>
+        private string CalculateSimpleObituary(uint mod, uint killerIndex, uint victimIndex)
+        {
+            LookupTable_uint modTable = GameConstants.MeansOfDeath_Protocol48;
+
+            if (demo.Protocol == Protocols.Protocol43 || demo.Protocol == Protocols.Protocol45)
+            {
+                // Convert mod to protocol 48.
+                mod = GameConstants.MeansOfDeath_Protocol43.Convert(mod, modTable);
+            }
+
+            string message = null;
+
+            // Suicide against world.
+            if (mod == modTable["MOD_SUICIDE"])
+                message = "suicide";
+            else if (mod == modTable["MOD_FALLING"])
+                message = "cratered";
+            else if (mod == modTable["MOD_CRUSH"])
+                message = "crushed";
+            else if (mod == modTable["MOD_WATER"])
+                message = "drowned";
+            else if (mod == modTable["MOD_SLIME"])
+                message = "slime";
+            else if (mod == modTable["MOD_LAVA"])
+                message = "lava";
+            else if (mod == modTable["MOD_TARGET_LASER"])
+                message = "laser";
+            else if (mod == modTable["MOD_TRIGGER_HURT"])
+                message = "hurt";
+
+            if (message != null)
+            {
+                return string.Format("{0} died ({1}).", playerNames[(int)victimIndex], message);
+            }
+
+            // Suicide using own weapon.
+            if (killerIndex == victimIndex)
+            {
+                // TODO: get correct gender.
+                Genders gender = Genders.Male;
+                string genderIdentifier = CalculateGenderIdentifier(gender);
+
+                if (mod == modTable["MOD_KAMIKAZE"])
+                    message = "kamikaze";
+                else if (mod == modTable["MOD_GRENADE_SPLASH"])
+                    message = string.Format("grenade", genderIdentifier);
+                else if (mod == modTable["MOD_ROCKET_SPLASH"])
+                    message = string.Format("rocket", genderIdentifier);
+                else if (mod == modTable["MOD_PLASMA_SPLASH"])
+                    message = string.Format("plasma", genderIdentifier);
+                else if (mod == modTable["MOD_BFG_SPLASH"])
+                    message = "bfg";
+                else if (mod == modTable["MOD_PROXIMITY_MINE"])
+                    message = "proximity mine";
+                else
+                    message = "unknown";
+            }
+
+            if (message != null)
+            {
+                return string.Format("{0} suicided ({1}).", playerNames[(int)victimIndex], message);
+            }
+
+            // Died (unknown killer).
+            if (killerIndex >= playerNames.Count)
+            {
+                return string.Format("{0} died.", playerNames[(int)victimIndex]);
+            }
+
+            // Killed.
+            string weapon = null;
+
+            if (mod == modTable["MOD_GRAPPLE"])
+                weapon = "Grappling Hook";
+            else if (mod == modTable["MOD_GAUNTLET"])
+                weapon = "Gauntlet";
+            else if (mod == modTable["MOD_MACHINEGUN"])
+                weapon = "Machinegun";
+            else if (mod == modTable["MOD_SHOTGUN"])
+                weapon = "Shotgun";
+            else if (mod == modTable["MOD_GRENADE"])
+                weapon = "Grenade Launcher";
+            else if (mod == modTable["MOD_GRENADE_SPLASH"])
+                message = "Grenade Launcher (splash damage)";
+            else if (mod == modTable["MOD_ROCKET"])
+                weapon = "Rocket Launcher";
+            else if (mod == modTable["MOD_ROCKET_SPLASH"])
+                weapon = "Rocket Launcher (splash damage)";
+            else if (mod == modTable["MOD_PLASMA"])
+                weapon = "Plasmagun";
+            else if (mod == modTable["MOD_PLASMA_SPLASH"])
+                weapon = "Plasmagun (splash damage)";
+            else if (mod == modTable["MOD_RAILGUN"])
+                weapon = "Railgun";
+            else if (mod == modTable["MOD_LIGHTNING"])
+                weapon = "Lightning Gun";
+            else if (mod == modTable["MOD_BFG"] || mod == modTable["MOD_BFG_SPLASH"])
+                weapon = "BFG";
+            else if (mod == modTable["MOD_NAIL"])
+                weapon = "Nailgun";
+            else if (mod == modTable["MOD_CHAINGUN"])
+                weapon = "Chaingun";
+            else if (mod == modTable["MOD_PROXIMITY_MINE"])
+                weapon = "Prox Mine";
+            else if (mod == modTable["MOD_KAMIKAZE"])
+                weapon = "Kamikaze blast";
+            else if (mod == modTable["MOD_JUICED"])
+                weapon = "Juice?";
+            else if (mod == modTable["MOD_TELEFRAG"])
+                weapon = "Telefrag";
+            else
+                message = "Unknown Weapon";
+
+            // Set string colour back to default (white) after each player name.
+            return string.Format("{0}^7 killed {1}^7 with {2}.", playerNames[(int)killerIndex], playerNames[(int)victimIndex], weapon);
+        }
+
+        /// <summary>
         /// Calculates a gender string identifier for a particular gender (e.g. "her" for female).
         /// </summary>
         private string CalculateGenderIdentifier(Genders gender)
@@ -300,22 +445,15 @@ namespace CDP.Quake3Arena.Analysis
 
             throw new ApplicationException("Unknown gender.");
         }
+        #endregion
 
         /// <summary>
-        /// Write a server command to the game log, stripping the command marker and trimming leading and trailing quotes and newline characters.
+        /// Is the specified entity type an event, and does the event match the specified event name.
         /// </summary>
-        /// <param name="commandMarker"></param>
-        /// <param name="command"></param>
-        private void GameLogWriteServerCommand(string commandMarker, string command)
-        {
-            GameLogWriteColourString(command.Remove(0, commandMarker.Length).Trim('"', '\n'));
-        }
-
-        /// <summary>
-        /// Is the specified entity eType an event, and if it is, is the event an obituary.
-        /// </summary>
-        /// <param name="eType">The entity eType.</param>
-        private bool IsObituary(uint eType)
+        /// <param name="eType">The entity type.</param>
+        /// <param name="eventName">The event name.</param>
+        /// <returns>True if the entity type is an event and the event matches the specified event name, otherwise false.</returns>
+        private bool IsEvent(uint eType, string eventName)
         {
             LookupTable_uint eTypesTable;
             LookupTable_uint entityEventsTable;
@@ -336,7 +474,7 @@ namespace CDP.Quake3Arena.Analysis
                 return false;
             }
 
-            return (eType - eTypesTable["ET_EVENTS"] == entityEventsTable["EV_OBITUARY"]);
+            return (eType - eTypesTable["ET_EVENTS"] == entityEventsTable[eventName]);
         }
 
         #region Command callbacks
@@ -361,11 +499,12 @@ namespace CDP.Quake3Arena.Analysis
 
             if (command.Command.StartsWith(chatServerCommandMarker))
             {
-                GameLogWriteServerCommand(chatServerCommandMarker, command.Command);
+                WriteServerCommand(gameLog, chatServerCommandMarker, command.Command);
+                WriteServerCommand(consoleLog, chatServerCommandMarker, command.Command);
             }
             else if (command.Command.StartsWith(printServerCommandMarker))
             {
-                GameLogWriteServerCommand(printServerCommandMarker, command.Command);
+                WriteServerCommand(consoleLog, printServerCommandMarker, command.Command);
             }
         }
 
@@ -378,11 +517,14 @@ namespace CDP.Quake3Arena.Analysis
                     continue;
                 }
 
-                if (IsObituary((uint)entity["eType"]))
+                uint eType = (uint)entity["eType"];
+
+                if (IsEvent(eType, "EV_OBITUARY"))
                 {
                     uint killerIndex = (uint?)entity["otherEntityNum2"] ?? 0;
                     uint victimIndex = (uint?)entity["otherEntityNum"] ?? 0;
-                    GameLogWriteColourString(CalculateObituary((uint)entity["eventParm"], killerIndex, victimIndex));
+                    WriteColouredString(gameLog, CalculateSimpleObituary((uint)entity["eventParm"], killerIndex, victimIndex));
+                    WriteColouredString(consoleLog, CalculateObituary((uint)entity["eventParm"], killerIndex, victimIndex));
                 }
             }
         }
