2d7a23f50299536217a5edc609abaeec5ebcdb6d
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-12-15 06:59:12 +0000

    [2.0] CDP.HalfLife: Removed empty InfoKeys value check; empty values are valid (but not null values).

--- code/plugins/CDP.HalfLife/Analysis/Player.cs
+++ code/plugins/CDP.HalfLife/Analysis/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using CDP.Core;
 
 namespace CDP.HalfLife.Analysis
 {
@@ -68,15 +69,21 @@ namespace CDP.HalfLife.Analysis
             InfoKeys = new List<InfoKey>();
         }
 
+        /// <summary>
+        /// Adds a value to an existing InfoKey, or creates a new InfoKey with the supplied key and value.
+        /// </summary>
+        /// <param name="key">The InfoKey key.</param>
+        /// <param name="value">The key's corresponding value. Can be empty.</param>
+        /// <param name="timestamp">The current timestamp.</param>
         public void AddInfoKeyValue(string key, string value, float timestamp)
         {
             if (string.IsNullOrEmpty(key))
             {
-                throw new ArgumentException("String cannot be empty or null.", "key");
+                throw new StringArgumentNullOrEmpty("key");
             }
-            if (string.IsNullOrEmpty(value))
+            if (value == null)
             {
-                throw new ArgumentException("String cannot be empty or null.", "value");
+                throw new ArgumentNullException("value");
             }
 
             InfoKey infoKey = InfoKeys.SingleOrDefault(ik => ik.Key == key);
