c0c95ccc7405b2a99da53e2d0bf5bf744cc0ac6c
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-12-15 04:25:33 +0000

    [2.0] CDP.QuakeLive: fixed Windows XP AppData path.

--- code/plugins/CDP.QuakeLive/Launcher.cs
+++ code/plugins/CDP.QuakeLive/Launcher.cs
@@ -1,24 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 using CDP.Core;
 
 namespace CDP.QuakeLive
 {
     public class Launcher : Core.Launcher
     {
+        // FOLDERID_LocalAppDataLow
+        // http://msdn.microsoft.com/en-us/library/bb762584%28VS.85%29.aspx
+        private readonly Guid LocalAppDataLow = new Guid("A520A1A4-1780-4FF6-BD18-167343C5AF16");
+
+        [DllImport("shell32.dll")]
+        static extern int SHGetKnownFolderPath([MarshalAs(UnmanagedType.LPStruct)] Guid rfid, uint dwFlags, IntPtr hToken, out IntPtr pszPath);
+
         private readonly ISettings settings = ObjectCreator.Get<ISettings>();
         private readonly IFileSystem fileSystem = ObjectCreator.Get<IFileSystem>();
         private readonly string demoFileName = "coldemoplayer.dm_73";
-        private readonly string localLow;
+        private readonly string appDataPath;
         private Demo demo;
 
         public Launcher()
         {
-            // FIXME: Windows XP
-            localLow = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData).Replace("Local", "LocalLow");
+            // There's no Environment.SpecialFolder for LocalLow. Use Vista/Windows 7 specific Win32 API function.
+            FileVersionInfo fv = FileVersionInfo.GetVersionInfo(fileSystem.PathCombine(Environment.GetEnvironmentVariable("windir"), "system32", "shell32.dll"));
+
+            // SHGetKnownFolderPath
+            // Minimum DLL Version: shell32.dll version 6.0.6000 or later.
+            // http://msdn.microsoft.com/en-us/library/bb762188%28VS.85%29.aspx
+            if (fv.FileMajorPart > 6 || (fv.FileMajorPart == 6 && fv.FileMinorPart > 0) || (fv.FileMajorPart == 6 && fv.FileMinorPart == 0 && fv.FileBuildPart >= 6000))
+            {
+                IntPtr path;
+                int result = SHGetKnownFolderPath(LocalAppDataLow, 0, IntPtr.Zero, out path);
+
+                if (result == 0)
+                {
+                    appDataPath = Marshal.PtrToStringUni(path);
+                    Marshal.FreeCoTaskMem(path);
+                }
+                else
+                {
+                    throw new ApplicationException(string.Format("SHGetKnownFolderPath failed with HRESULT \'{0}\'.", result));
+                }
+            }
+            else
+            {
+                appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            }
         }
 
         public override void Initialise(Core.Demo demo)
@@ -29,12 +58,12 @@ namespace CDP.QuakeLive
 
         public override string CalculateDestinationFileName()
         {
-            return fileSystem.PathCombine(localLow, "id Software", "quakelive", "home", "baseq3", "demos", demoFileName);
+            return fileSystem.PathCombine(appDataPath, "id Software", "quakelive", "home", "baseq3", "demos", demoFileName);
         }
 
         public override bool Verify()
         {
-            string demosFolderPath = fileSystem.PathCombine(localLow, "id Software", "quakelive", "home", "baseq3", "demos");
+            string demosFolderPath = fileSystem.PathCombine(appDataPath, "id Software", "quakelive", "home", "baseq3", "demos");
 
             if (!Directory.Exists(demosFolderPath))
             {
