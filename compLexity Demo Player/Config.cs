eca0be8e3987c23e4a3bd22a1b7f226ab92077b8
Jonathan Young <young.jpc@gmail.com>
2013-12-18 21:28:33 +1000

    Updated JsonExSerializer to 3.1.1.283. Properties in config.json that don't exist in code no longer cause an exception to be thrown and thus the config values reset to default.

--- compLexity Demo Player/Config.cs
+++ compLexity Demo Player/Config.cs
@@ -31,7 +31,6 @@ namespace compLexity_Demo_Player
         public System.Windows.WindowState WindowState { get; set; }
         public Double WindowWidth { get; set; }
         public Double WindowHeight { get; set; }
-        public Double ExplorerPaneWidth { get; set; } // unused legacy property. here to prevent json deserializer warnings.
         public Double DemoListPaneHeight { get; set; }
 
         // steam
